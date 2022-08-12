80115be0b7948dd94f489688dde92d8e571115b0
Robert <roberthklink@gmail.com>
2022-08-12 16:53:13 +0200

    cmake stuff

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rom/Documents/cpp/crisps-8
-# Build directory: /home/rom/Documents/cpp/crisps-8/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
