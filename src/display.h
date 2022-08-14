bdbaaa79fcf3e01da5b9437727fca72887fd5a80
Robert <roberthklink@gmail.com>
2022-08-14 22:33:01 +0200

    Revert "Revert "Basics of display working""
    This reverts commit eb6c3af5ace5074de0e477091a2ed55e5b61e6e4.

--- src/display.h
+++ src/display.h
@@ -1,8 +1,18 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
+#include <array>
+#include "chip_8_definitions.h"
 
-int init_window(SDL_Window* window);
+class Display
+{
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Texture* texture;
+public:
+    Display(int scale = 8);
+    ~Display(void);
+    void update(std::array<WORD, (64*32)> display);
+};
 
 #endif
