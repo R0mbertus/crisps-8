3cc682f503b07f556d59e361a58c6d54010db95e
Robert <roberthklink@gmail.com>
2022-12-27 21:19:57 +0100

    Merge attempt

--- src/chip_8.cpp
+++ src/chip_8.cpp
@@ -8,7 +8,7 @@
 
 Chip8::Chip8() {
     for (size_t i = 0; i < kFontsetSize; i++) {
-        memory_[i + kFontsetSize] = kFontset[i];
+        memory_[kFontsetSize + i] = kFontset[i];
     }
 
     srand(time(nullptr));
@@ -40,7 +40,12 @@ SHORT Chip8::nibble(SHORT val, SHORT val_to_binary_and, int bits) {
 
 void Chip8::setKey(BYTE key, BYTE state) {
     keypad_[key] = state;
-    latest_key_ = key;
+    if (state) {
+        latest_key_ = key;
+    }
+    else {
+        latest_key_ = 0xFF;
+    }
 }
 
 void Chip8::instructions() {
@@ -93,7 +98,7 @@ void Chip8::instructions() {
             break;
         }
         case 0x7: {
-            v_registers_[x] = (v_registers_[x] + kk) & 0xFF;
+            v_registers_[x] += kk;
             break;
         }
         case 0x8: {
@@ -127,7 +132,7 @@ void Chip8::instructions() {
                 }
                 case 0x6: {
                     v_registers_[0xF] = v_registers_[x] & 0x1;
-                    v_registers_[x] = v_registers_[y] >> 1;
+                    v_registers_[x] >>= 1;
                     break;
                 }
                 case 0x7: {
@@ -137,7 +142,7 @@ void Chip8::instructions() {
                 }
                 case 0xE: {
                     v_registers_[0xF] = v_registers_[x] >> 7;
-                    v_registers_[x] = v_registers_[y] << 1;
+                    v_registers_[x] <<= 1;
                     break;
                 }
             }
@@ -156,7 +161,7 @@ void Chip8::instructions() {
             break;
         }
         case 0xC: {
-            v_registers_[x] = kk & (rand() % 0xFF);
+            v_registers_[x] = kk & (rand() % 255);
             break;
         }
         case 0xD: {
@@ -167,18 +172,14 @@ void Chip8::instructions() {
             const BYTE x_pos = v_registers_[x] % kHeight;
             const BYTE y_pos = v_registers_[y] % kWidth;
             
-            for (size_t i = 0; i < height; i++) {
-                BYTE sprite = memory_[I_ + i];
-                const WORD row = (y_pos + i) % kWidth;
-                for(size_t j = 0; j < 8; j++) {
-                    const BYTE pixel = (sprite & 0x80U) >> 7;
-                    const WORD col = (x_pos + j) % kHeight;
-                    const WORD offset = row * 64 + col;
+            for (BYTE row = 0; row < height; row++) {
+                WORD const data = memory_[I_ + row];
+                for(unsigned int column = 0; column < 8; column++) {
+                    const BYTE pixel = data & (0x80U >> column);
                     if (pixel) {
-                        v_registers_[0xF] |= static_cast<BYTE>(display_[offset]);
-                        display_[(y_pos + row) * kHeight + (x_pos + col)] ^= 0xFFFFFFFF;
+                        v_registers_[0xF] |= static_cast<BYTE>(display_[(y_pos + row) * kHeight + (x_pos + column)] == 1);
+                        display_[(y_pos + row) * kHeight + (x_pos + column)] ^= 0xFFFFFFFF;
                     }
-                    sprite <<= 1;
                 }
             }
             break;
